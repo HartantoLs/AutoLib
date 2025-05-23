9f3201960e81390654c15bbab4918fa7e1042ed2
k3rlyn <kerlyndesliaa@gmail.com>
2025-05-23 07:36:32 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -459,13 +459,14 @@ export default function HomePage() {
 
       {/* Footer */}
       <footer className="bg-white/80 backdrop-blur-sm mt-auto relative z-10">
-        {/* <div className="max-w-7xl mx-auto py-8 md:py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
+        <div className="max-w-7xl mx-auto py-8 md:py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
           <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
             <div className="px-5 py-2">
               <Link href="/books" className="text-sm sm:text-base text-indigo-500 hover:text-indigo-600 transition duration-300">
                 Books
               </Link>
-            </div> */}
+            </div>
+        
             <div className="px-5 py-2">
               <Link href="/about" className="text-sm sm:text-base text-indigo-500 hover:text-indigo-600 transition duration-300">
                 About
